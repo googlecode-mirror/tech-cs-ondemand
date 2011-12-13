83e43212628d7d21df4bcbef6cec52b24f924e22
josephgkim1221 <josephgkim1221@gmail.com>
2011-12-13 07:04:54 +0000

    Facebook Open Graph meta headers

--- src0.1/scripts/HTML_top1.php
+++ src0.1/scripts/HTML_top1.php
@@ -9,4 +9,32 @@
 <head>
 
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
+<?php
+	$meta_description = "Tech OnDemand is an open repository of academic resources aimed to educate anyone who is interested in the various technical topics offered at the Georgia Institute of Technology.";
+	
+	echo '<meta name="description" content="'.$meta_description.'" />';
+	
+	connectToDb();
+	if (isset($_GET['pid'])) // post.php
+	{
+		$Q = mysql_query("SELECT * FROM `PostCollection1332` WHERE `postid`=".$_GET['pid']);
+		$Q = mysql_fetch_array($Q);
+		$Q = $Q['title'];
+	}
+	else if (isset($_GET['cid'])) // class.php
+	{
+		$Q = mysql_query("SELECT * FROM `ClassCollection` WHERE `classid`=".$_GET['cid']);
+		$Q = mysql_fetch_array($Q);
+		$Q = $Q['subject'] .' '. $Q['number'] .': '. $Q['title'];
+	}
+	else // index.php & al (no GET vars)
+	{
+		$Q = "Tech OnDemand";
+	}
+	
+	echo '<meta property="og:title" content="'.addslashes($Q).'" />';
+	echo '<meta property="og:description" content="'.$meta_description.'" />';
+?>
+	<meta property="og:image" content="http://www.tech-ondemand.com/images/square.jpg" />
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
